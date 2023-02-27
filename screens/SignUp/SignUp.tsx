cd579c587b5016cdba4ebeed7d9559a981c2793c
Yash Shah <62358049+git-yash@users.noreply.github.com>
2023-02-27 13:30:56 -0600

    Fixed previous commit

--- screens/SignUp/SignUp.tsx
+++ screens/SignUp/SignUp.tsx
@@ -1,6 +1,3 @@
-import {Box, Button, Text} from 'native-base';
-import React from 'react';
-import {SafeAreaView} from 'react-native';
 import {Box, Button, StatusBar, Text} from 'native-base';
 import React, { useState } from 'react';
 import {SafeAreaView, TextInputBase} from 'react-native';
@@ -13,18 +10,17 @@ const SignUp = (props: {navigation: any}) => {
   const [emailText, setEmailText] = useState();
 
   return (
-    <Box>
+    <><Box>
       <Text>Sign Up Screen</Text>
-    </Box>
-    <>
-      <SafeAreaView style={{backgroundColor: '#222222', flex: 1}}>
-        <Text style={{fontSize: 25, paddingTop: 100}}>Hello! Register to get started</Text>
+    </Box><>
+        <SafeAreaView style={{ backgroundColor: '#222222', flex: 1 }}>
+          <Text style={{ fontSize: 25, paddingTop: 100 }}>Hello! Register to get started</Text>
 
-        <Box style={{justifyContent: 'space-evenly', paddingTop: 20, alignItems: 'center'}}>
-          <CustomTextInput placeholderText={'Test'} value={emailText} onChangeText={(newText: any) => setEmailText(newText)}/>
-        </Box>
-      </SafeAreaView>
-    </>
+          <Box style={{ justifyContent: 'space-evenly', paddingTop: 20, alignItems: 'center' }}>
+            <CustomTextInput placeholderText={'Test'} value={emailText} onChangeText={(newText: any) => setEmailText(newText)} />
+          </Box>
+        </SafeAreaView>
+      </></>
   );
 };
 
