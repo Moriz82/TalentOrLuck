a95622b5a58ca2077e2edb3a5d4826a37925b215
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2023-02-27 13:29:21 -0600

    Worked on SignUp screen
    I added some labels and views

--- screens/SignUp/SignUp.tsx
+++ screens/SignUp/SignUp.tsx
@@ -1,12 +1,30 @@
 import {Box, Button, Text} from 'native-base';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {Box, Button, StatusBar, Text} from 'native-base';
+import React, { useState } from 'react';
+import {SafeAreaView, TextInputBase} from 'react-native';
+import { CustomTextInput } from "../../components/CustomTextInput";
+
+// ZANE IS WORKING ON THIS
+// line height and height to fix labels
 
 const SignUp = (props: {navigation: any}) => {
+  const [emailText, setEmailText] = useState();
+
   return (
     <Box>
       <Text>Sign Up Screen</Text>
     </Box>
+    <>
+      <SafeAreaView style={{backgroundColor: '#222222', flex: 1}}>
+        <Text style={{fontSize: 25, paddingTop: 100}}>Hello! Register to get started</Text>
+
+        <Box style={{justifyContent: 'space-evenly', paddingTop: 20, alignItems: 'center'}}>
+          <CustomTextInput placeholderText={'Test'} value={emailText} onChangeText={(newText: any) => setEmailText(newText)}/>
+        </Box>
+      </SafeAreaView>
+    </>
   );
 };
 
